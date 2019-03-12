b07475f5caa62c034e4e7c7dd449fa6afcf7c859
zhangqi001129 <2843275748@qq.com>
2019-03-12 13:46:10 +0000

    test88

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-//tset666777
+//tset8888
     $server_name=[
         '192.168.131.29',
     ];
     foreach($server_name as $k=>$v){
-        $cmd= 'ssh '.$v .'  cd /home/wwwroot/fabu && git pull';
+        $cmd= 'ssh '.$v .' " cd /home/wwwroot/fabu ; git pull" ';
         echo $cmd;echo '<hr>';
         $result= shell_exec($cmd);
     }
